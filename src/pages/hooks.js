816b6b70357562130ac0a4970441f97060514320
thuqnguyen <thuqnguyen@kms-technology.com>
2020-04-09 09:51:08 +0700

    useEffect

--- src/pages/hooks.js
+++ src/pages/hooks.js
@@ -1,12 +1,23 @@
-import { useState } from "react";
+import { useState, useEffect } from 'react';
 
 function HookExamples() {
   const [count, setCount] = useState(0);
+  const [count2, setCount2] = useState(0);
+  useEffect(() => {
+    document.title = `You clicked ${count} times`;
+
+    // clean up
+    return () => {
+      alert('clean up...');
+    };
+  }, [count]);
 
   return (
     <div>
       <p>You clicked {count} times</p>
+      <p>You clicked 2 {count2} times</p>
       <button onClick={() => setCount(count + 1)}>Click me</button>
+      <button onClick={() => setCount2(count2 + 1)}>Click me 2</button>
     </div>
   );
 }
